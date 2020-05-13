69cb6a25f016d4e3d36772c9e7a8ee34a31587c9
Leandro Nicolas <oleito@gmail.com>
2020-05-12 22:23:55 -0300

    Agrega modelos al v-for

--- js/main.js
+++ js/main.js
@@ -2,10 +2,22 @@ new Vue({
   el: "#app",
   data: {
     modelos: [
-      { modelo: "modelo" },
-      { modelo: "modelo" },
-      { modelo: "modelo" },
-      { modelo: "modelo" }
+      { modelo: "Wave 110S", img: "WAVE110S.jpg" , ficha: "WAVE110S.pdf"},
+      { modelo: "Biz 125", img: "BIZ125.jpg" },
+      { modelo: "CG150 Titan", img: "CG150TITAN.jpg" , ficha: "CG150T.pdf"},
+      { modelo: "Elite Tricolor", img: "ELITE.jpg" },
+      { modelo: "PCX 150", img: "PCX150.jpg" },
+      { modelo: "XR 150L", img: "XR150L.jpg" , ficha: "XR150L.pdf"},
+      { modelo: "XR 190L", img: "XR190L.jpg" },
+      { modelo: "XR 250 Tornado", img: "XR250.jpg" , ficha: "XR250T.pdf"},
+      { modelo: "XRE300 Rally", img: "XRE300.jpg" , ficha: "XRE300.pdf"},
+      { modelo: "CB125 Twister", img: "CB125.jpg" , ficha: "CB125T.pdf"},
+      { modelo: "CB250 Twister", img: "CB250.jpg" , ficha: "CB250T.pdf"},
+      { modelo: "CB500F", img: "CB500F.jpg" },
+      { modelo: "CB1000R", img: "CB1000R.jpg" },
+      { modelo: "NC750X", img: "NC750X.jpg" },
+      { modelo: "Africa Twin", img: "AFRICA.jpg" , ficha: "AFRICA.pdf"},
+      // { modelo: "CB500X", img: "CB500X.jpg" },
     ]
   }
 });
